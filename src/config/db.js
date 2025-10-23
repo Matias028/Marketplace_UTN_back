149cad88c9c232f9e40294e9fcdd0b27be1616ae
Matias Gonzalez <mg_mdp@hotmail.com>
2025-10-23 18:41:36 -0300

    back listo para desplegar

--- src/config/db.js
+++ src/config/db.js
@@ -7,9 +7,10 @@ const sequelize = new Sequelize(
   process.env.DB_USER,
   process.env.DB_PASS,
   {
-    host: process.env.DB_HOST || 'localhost',
-    dialect: 'mysql',
+    host: process.env.DB_HOST ,
+    dialect: process.env.DB_DIALECT ,
     logging: false,
+    port: process.env.DB_PORT,
   }
 );
 

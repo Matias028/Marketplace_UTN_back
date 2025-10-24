d1e6f9481ac844421031d9d5fdb3afffd7627173
Matias Gonzalez <mg_mdp@hotmail.com>
2025-10-23 21:53:51 -0300

    back listo para desplegar

--- src/config/db.js
+++ src/config/db.js
@@ -13,7 +13,7 @@ for (const key of requiredEnv) {
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
-  process.env.DB_PASSWORD,
+  process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
     dialect: 'mysql',

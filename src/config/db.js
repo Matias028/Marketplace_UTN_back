e29fe77eed0c6941a3acaf27b370cfde8b003d74
Matias Gonzalez <mg_mdp@hotmail.com>
2025-10-23 21:43:42 -0300

    back listo para desplegar

--- src/config/db.js
+++ src/config/db.js
@@ -2,16 +2,18 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST ,
-    dialect: process.env.DB_DIALECT ,
-    logging: false,
-    port: process.env.DB_PORT,
-  }
-);
+
+
+
+const mysql2 = await import('mysql2'); 
+const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
+  dialect: 'mysql',
+  dialectModule: mysql2,
+});
+
 
 export default sequelize;
+
+
+
